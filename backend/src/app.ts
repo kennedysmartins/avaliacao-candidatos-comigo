988f05b2f489a48d970569a8e4f621706da12935
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-12 21:52:39 -0300

    feat: :sparkles: add bodyParser and link routes in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,21 @@
-import express, { Express, Request, Response } from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import express, { Express } from "express";
+
+import ticketRoutes from './routes/ticket.routes';
+import statusRoutes from './routes/status.routes';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express Server");
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use('/tickets', ticketRoutes)
+app.use('/status', statusRoutes)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
