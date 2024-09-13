3420ac34110287b5ec73e35143522dd077b53fd9
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-13 00:19:30 -0300

    style: :lipstick: added color variable, images and logo and font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
+import { Roboto } from 'next/font/google';
 import "./globals.css";
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: "Ticket Management System",
@@ -12,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
-        className={`antialiased`}
+        className={`antialiased ${roboto.className}`}
       >
         {children}
       </body>
