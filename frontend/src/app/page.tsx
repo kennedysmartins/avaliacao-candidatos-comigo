c2d8c59a8438d3555a3ce78c4bb70178fe3fe9bd
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-13 00:24:27 -0300

    feat: :sparkles: new home page for testing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,22 @@
+import Breadcrumbs from "@/components/Breadcrumbs";
+import Button from "@/components/Button";
+import Filters from "@/components/Filters";
+import NavBar from "@/components/NavBar";
+import Separator from "@/components/Separator";
+import TicketTable from "@/components/Table";
+import { BsPlus } from "react-icons/bs";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <NavBar pageTitle="Atendimento ao cliente" />
+      <Breadcrumbs pageTitle="Atendimento ao cliente" />
+      <Separator />
+      <div className="flex gap-2 justify-start items-center px-8">
+        <Button variant="filled" iconPosition="right" icon={<BsPlus size={20} />} >Abrir ticket</Button> 
+        <Filters />
+      </div>
+        <TicketTable />
     </div>
   );
 }
