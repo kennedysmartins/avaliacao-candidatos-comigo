9add92ef32723caddaaff0592052c2d572af85ec
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-15 20:26:31 -0300

    refactor: :sparkles: refactor auth controller to return more info in jwt

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,55 +1,64 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import prisma from "../lib/prisma";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { User } from '@prisma/client';
+import { User } from "@prisma/client";
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { email, password } = req.body;
-        const user: User | null = await prisma.user.findUnique({
-            where: {
-                email,
-            },
-        });
-        if (!user) {
-            res.status(401).json({ error: 'Invalid credentials' });
-            return;
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            res.status(401).json({ error: 'Invalid credentials' });
-            return;
-        }
-        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-        res.status(200).json({ user, token });
+  try {
+    const { email, password } = req.body;
+    const user: User | null = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      res.status(401).json({ error: "Invalid credentials" });
+      return;
     }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(401).json({ error: "Invalid credentials" });
+      return;
     }
-}
+    const token = jwt.sign(
+      { userId: user.id, role: user.role, name: user.name, email: user.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "24h" }
+    );
+    res.status(200).json(token);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-export const verifyUserToken = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            res.status(401).json({ error: 'Unauthorized' });
-            return;
-        }
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: string };
-        const user: User | null = await prisma.user.findUnique({
-            where: {
-                id: decodedToken.userId,
-            },
-        });
-        if (!user) {
-            res.status(401).json({ error: 'Unauthorized' });
-            return;
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+export const verifyUserToken = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
     }
+    const decodedToken = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as { userId: string };
+    const user: User | null = await prisma.user.findUnique({
+      where: {
+        id: decodedToken.userId,
+      },
+    });
+    if (!user) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
