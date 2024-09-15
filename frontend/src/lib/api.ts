bbf94ca6dfce3d3192da9ef181a6677935eeca06
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-15 19:29:17 -0300

    feat: :sparkles: update api requests

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,135 +1,156 @@
-"use client"
-import axios from 'axios';
-import { getTicketsOutput, Ticket, User } from './types';
+"use server";
+import axios from "axios";
+import axiosInstance from "./axiosInstance";
+import { getTicketsOutput, Ticket, User } from "./types";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000';
-
-export const login = async (credentials: { email: string; password: string }): Promise<{ data: { token: string } }> => {
+const API_BASE_URL = process.env.BACKEND_URL || "http://localhost:4000";
+export const loginRequest = async (credentials: {
+  email: string;
+  password: string;
+}): Promise<{ data: string } | { data: null }> => {
   try {
-    return await axios.post(`${API_BASE_URL}/auth/login`, credentials);
+    const response = await axios.post(
+      `${API_BASE_URL}/auth/login`,
+      credentials
+    );
+    if (typeof response.data === "string") {
+      return { data: response.data };
+    } else {
+      throw new Error("Invalid response format");
+    }
   } catch (error) {
-    console.error('Login error:', error);
+    console.error("Login error:", error);
+    return { data: null };
+  }
+};
+
+export const createUser = async (
+  userData: Omit<User, "id" | "tickets">
+): Promise<{ data: User }> => {
+  try {
+    return await axiosInstance.post(`${API_BASE_URL}/users`, userData);
+  } catch (error) {
+    console.error("Create user error:", error);
     throw error;
   }
 };
 
-export const verifyToken = async (token: string): Promise<{ data: { valid: boolean } }> => {
+export const getUsers = async (): Promise<{ data: User[] }> => {
   try {
-    return await axios.post(`${API_BASE_URL}/auth/verifyToken`, { token });
+    return await axiosInstance.get(`${API_BASE_URL}/users`);
   } catch (error) {
-    console.error('Token verification error:', error);
+    console.error("Get users error:", error);
     throw error;
   }
 };
 
-// User endpoints
-export const createUser = async (userData: Omit<User, 'id' | 'tickets'>, token: string): Promise<{ data: User }> => {
+export const getUserById = async (id: string): Promise<{ data: User }> => {
   try {
-    return await axios.post(`${API_BASE_URL}/users`, userData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    return await axiosInstance.get(`${API_BASE_URL}/users/${id}`);
   } catch (error) {
-    console.error('Create user error:', error);
+    console.error("Get user by ID error:", error);
     throw error;
   }
 };
 
-export const getUsers = async (token: string): Promise<{ data: User[] }> => {
+export const updateUser = async (
+  id: string,
+  userData: Partial<Omit<User, "id" | "tickets">>
+): Promise<{ data: User }> => {
   try {
-    return await axios.get(`${API_BASE_URL}/users`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    return await axiosInstance.put(`${API_BASE_URL}/users/${id}`, userData);
   } catch (error) {
-    console.error('Get users error:', error);
+    console.error("Update user error:", error);
     throw error;
   }
 };
 
-export const getUserById = async (id: string, token: string): Promise<{ data: User }> => {
+export const deleteUser = async (id: string): Promise<User> => {
   try {
-    return await axios.get(`${API_BASE_URL}/users/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    return await axiosInstance.delete(`${API_BASE_URL}/users/${id}`);
   } catch (error) {
-    console.error('Get user by ID error:', error);
+    console.error("Delete user error:", error);
     throw error;
   }
 };
 
-export const updateUser = async (id: string, userData: Partial<Omit<User, 'id' | 'tickets'>>, token: string): Promise<{ data: User }> => {
+export const getTickets = async ({
+  queryParams,
+}: {
+  queryParams: string;
+}): Promise<getTicketsOutput> => {
   try {
-    return await axios.put(`${API_BASE_URL}/users/${id}`, userData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await axiosInstance.get(
+      `${API_BASE_URL}/tickets?${queryParams}`
+    );
+    return {
+      tickets: response.data.tickets,
+      count: response.data.count,
+      page: response.data.page,
+      totalPages: response.data.totalPages,
+      pageSize: response.data.pageSize,
+    };
   } catch (error) {
-    console.error('Update user error:', error);
+    console.error("Get tickets error:", error);
     throw error;
   }
 };
 
-export const deleteUser = async (id: string, token: string): Promise<User> => {
+export const getTicketById = async (id: string): Promise<{ data: Ticket }> => {
   try {
-    return await axios.delete(`${API_BASE_URL}/users/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    return await axiosInstance.get(`${API_BASE_URL}/tickets/${id}`);
   } catch (error) {
-    console.error('Delete user error:', error);
+    console.error("Get ticket by ID error:", error);
     throw error;
   }
 };
 
-export const getTickets = async ({queryParams, token}:{queryParams: string, token: string}): Promise<getTicketsOutput> => {
+export const createTicket = async (
+  ticketData: Omit<
+    Ticket,
+    "id" | "createdAt" | "updatedAt" | "deletedAt" | "status"
+  >
+): Promise<Ticket> => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/tickets?${queryParams}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    return { tickets: response.data.tickets, count: response.data.count, page: response.data.page, totalPages: response.data.totalPages, pageSize: response.data.pageSize };
+    console.log("Creating ticket with data:", ticketData);
+    const response = await axiosInstance.post(
+      `${API_BASE_URL}/tickets`,
+      ticketData
+    );
+    console.log(response.data);
+    return response.data;
   } catch (error) {
-    console.error('Get tickets error:', error);
+    console.error("Create ticket error:", error);
     throw error;
   }
 };
 
-export const getTicketById = async (id: string, token: string): Promise<{ data: Ticket }> => {
+export const updateTicket = async (
+  id: string,
+  ticketData: Partial<
+    Omit<Ticket, "id" | "createdAt" | "updatedAt" | "deletedAt">
+  >
+): Promise<Ticket> => {
   try {
-    return await axios.get(`${API_BASE_URL}/tickets/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    console.log("Updating ticket with data:", ticketData);
+    const response = await axiosInstance.put(
+      `${API_BASE_URL}/tickets/${id}`,
+      ticketData
+    );
+    return response.data;
   } catch (error) {
-    console.error('Get ticket by ID error:', error);
+    console.error("Update ticket error:", error);
     throw error;
   }
 };
 
-export const createTicket = async (ticketData: Omit<Ticket, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>, token: string): Promise<{ data: Ticket }> => {
+export const deleteTicket = async (
+  id: string
+): Promise<{ data: { success: boolean } }> => {
   try {
-    return await axios.post(`${API_BASE_URL}/tickets`, ticketData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    return await axiosInstance.delete(`${API_BASE_URL}/tickets/${id}`);
   } catch (error) {
-    console.error('Create ticket error:', error);
-    throw error;
-  }
-};
-
-export const updateTicket = async (id: string, ticketData: Partial<Omit<Ticket, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>>, token: string): Promise<{ data: Ticket }> => {
-  try {
-    return await axios.put(`${API_BASE_URL}/tickets/${id}`, ticketData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  } catch (error) {
-    console.error('Update ticket error:', error);
-    throw error;
-  }
-};
-
-export const deleteTicket = async (id: string, token: string): Promise<{ data: { success: boolean } }> => {
-  try {
-    return await axios.delete(`${API_BASE_URL}/tickets/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  } catch (error) {
-    console.error('Delete ticket error:', error);
+    console.error("Delete ticket error:", error);
     throw error;
   }
 };
