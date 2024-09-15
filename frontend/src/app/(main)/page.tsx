27154f61ca4071e8fb3adaf4dc613255184f7955
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-15 19:20:04 -0300

    style: :lipstick: styles update

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -5,10 +5,14 @@ import { MdDashboard } from "react-icons/md";
 export default function Home() {
   return (
     <div>
-      <main className="container mx-8 mt-8">
-        <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-        <Link href="/atendimento-ao-cliente">
-          <Button variant="filled" iconPosition="left" icon={<MdDashboard size={20} />}>
+      <main className='container mx-8 mt-8'>
+        <h1 className='text-3xl font-bold mb-4'>Dashboard</h1>
+        <Link href='/atendimento-ao-cliente'>
+          <Button
+            variant='filled'
+            iconPosition='left'
+            icon={<MdDashboard size={20} />}
+          >
             Atendimento ao Cliente
           </Button>
         </Link>
