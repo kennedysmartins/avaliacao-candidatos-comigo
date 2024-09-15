f0b48fe04717e9318fdd72020e0dbf1472007dd3
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-15 17:02:52 -0300

    fix: :bug: fix middleware env key

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,17 +1,24 @@
-// authMiddleware.ts
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const secret = process.env.JSON_SECRET as string; 
+const secret = process.env.JWT_SECRET as string;
+
+if (!secret) {
+  console.error('Secret key is not defined');
+}
 
 interface CustomRequest extends Request {
   user?: string | JwtPayload;
 }
 
 const verifyToken = (req: CustomRequest, res: Response, next: NextFunction) => {
-  const token = req.cookies?.token || req.headers['authorization']?.replace('Bearer ', '');
+  const authHeader = req.headers['authorization'] as string;
+  const token = req.cookies?.token || (authHeader ? authHeader.replace('Bearer ', '') : undefined);
 
   if (!token) {
+    console.log('No token provided');
     return res.status(401).json({ message: 'Access Denied' });
   }
 
@@ -20,6 +27,7 @@ const verifyToken = (req: CustomRequest, res: Response, next: NextFunction) => {
     req.user = verified;
     next();
   } catch (err) {
+    console.error('Token verification error:', err);
     res.status(400).json({ message: 'Invalid Token', error: err });
   }
 };
