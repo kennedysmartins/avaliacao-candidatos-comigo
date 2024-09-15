ee9c8dcdae884ee3feadaec6703426c94ec68ba8
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-15 17:01:19 -0300

    refactor: :seedling: change seed logic

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,5 +1,9 @@
-// src/prisma/seed.ts
-import { ContactType, PrismaClient, TicketStatus } from "@prisma/client";
+import {
+  ContactType,
+  PrismaClient,
+  TicketStatus,
+  TicketType,
+} from "@prisma/client";
 import * as bcrypt from "bcrypt";
 
 /* 
@@ -8,7 +12,12 @@ import * as bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
-async function createUser(email: string, name: string, password: string, role: "ADMIN" | "ATTENDANT") {
+async function createUser(
+  email: string,
+  name: string,
+  password: string,
+  role: "ADMIN" | "ATTENDANT"
+) {
   return prisma.user.upsert({
     where: { email },
     update: {},
@@ -25,12 +34,70 @@ function getRandomElement<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function getRandomReason() {
-  const reasons = ["Incidente", "Upgrade", "Teste", "Furto", "Danificado", "Instalação"];
-  return getRandomElement(reasons);
-}
-
-function getRandomDescription() {
+async function createTickets(
+  adminUserId: string,
+  attendantUserId: string,
+  quantity: number = 30
+) {
+  const customers = [
+    "Kennedy",
+    "Renata",
+    "Javi",
+    "Rafael",
+    "Hanna",
+    "Lucas",
+    "Amanda",
+    "Joyce",
+    "Deriki",
+    "Rodrigo",
+    "Camila",
+    "Thiago",
+  ];
+  const vehicles = [
+    "Logan",
+    "Punto",
+    "Gol",
+    "Fusca",
+    "C4",
+    "HB20",
+    "Onix",
+    "Chevette",
+  ];
+  const reasons = [
+    "Testes de Instalação",
+    "Melhorias no veículo",
+    "Nova Instalação",
+    "Aparelho corrompido",
+    "Manutenção Regular",
+    "Problema de software",
+    "Calibração de sensores",
+    "Substituição de bateria",
+    "Configuração de alertas",
+    "Treinamento do usuário",
+    "Integração com outros sistemas",
+    "Diagnóstico de falhas",
+    "Atualização de mapas",
+    "Reparo de antena",
+    "Otimização de rotas",
+    "Configuração de geofence",
+    "Análise de dados de telemetria",
+    "Atualização de firmware",
+    "Resolução de problemas de conectividade",
+    "Personalização de relatórios",
+    "Configuração de alertas de manutenção",
+    "Otimização de consumo de combustível",
+    "Instalação de acessórios",
+    "Configuração de perfis de motorista",
+    "Resolução de conflitos de software",
+  ];
+  const contactTypes: ContactType[] = ["EMAIL", "PHONE", "CHAT", "IN_PERSON"];
+  const statuses: TicketStatus[] = ["OPEN", "IN_PROGRESS", "CLOSED"];
+  const types: TicketType[] = [
+    "OPERATIONAL",
+    "SUPPORT",
+    "RELATIONSHIP",
+    "SALES",
+  ];
   const descriptions = [
     "Testes de instalação",
     "Veículo sem comunicação",
@@ -39,31 +106,31 @@ function getRandomDescription() {
     "Aparelho corrompido",
     "Nova instalação",
   ];
-  return getRandomElement(descriptions);
-}
-
-async function createTickets(adminUserId: string, attendantUserId: string, quantity: number = 30) {
-  const customers = ["Kennedy", "Renata", "Rafael", "Vitória", "Lucas", "Amanda", "Joyce", "Deriki", "Rodrigo", "Camila"];
-  const vehicles = ["Logan", "Punto", "Gol", "Fusca", "C4", "Hb20", "Onix", "Chevette"];
-  const contactTypes: ContactType[] = ["EMAIL", "PHONE", "CHAT", "IN_PERSON"];
-  const statuses: TicketStatus[] = ["OPEN", "IN_PROGRESS", "CLOSED"];
 
   const tickets = Array.from({ length: quantity }, async () => {
     const assignedToId = Math.random() > 0.5 ? adminUserId : attendantUserId;
 
     return prisma.ticket.create({
       data: {
-        type: Math.random() > 0.5 ? "SUPPORT" : "SALES",
-        reason: getRandomReason(),
-        description: getRandomDescription(),
+        type: getRandomElement(types),
+        reason: getRandomElement(reasons),
+        description: getRandomElement(descriptions),
         customer: getRandomElement(customers),
         vehicle: getRandomElement(vehicles),
         passiveContact: Math.random() > 0.5,
         contactType: getRandomElement(contactTypes),
         status: getRandomElement(statuses),
         assignedTo: { connect: { id: assignedToId } },
-        createdAt: new Date(new Date().setDate(new Date().getDate() - Math.floor(Math.random() * 7))),
-        deadline: new Date(new Date().setDate(new Date().getDate() + Math.floor(Math.random() * 7) + 1)),
+        createdAt: new Date(
+          new Date().setDate(
+            new Date().getDate() - Math.floor(Math.random() * 7)
+          )
+        ),
+        deadline: new Date(
+          new Date().setDate(
+            new Date().getDate() + Math.floor(Math.random() * 7) + 1
+          )
+        ),
       },
     });
   });
@@ -72,8 +139,18 @@ async function createTickets(adminUserId: string, attendantUserId: string, quant
 }
 
 async function main() {
-  const adminUser = await createUser("admin@comigo.com", "Admin", "admin", "ADMIN");
-  const attendantUser = await createUser("atendente@comigo.com", "Atendente", "atendente", "ATTENDANT");
+  const adminUser = await createUser(
+    "admin@comigo.com",
+    "Admin",
+    "admin",
+    "ADMIN"
+  );
+  const attendantUser = await createUser(
+    "atendente@comigo.com",
+    "Atendente",
+    "atendente",
+    "ATTENDANT"
+  );
 
   await createTickets(adminUser.id, attendantUser.id, 30);
 
