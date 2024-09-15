31bfefa16f1e4713133a7a55ad87dc4407b895b2
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-15 01:10:42 -0300

    fix: :bug: fix onclick event and change styles

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,23 +5,34 @@ interface ButtonProps {
   iconPosition?: 'left' | 'right'
   icon?: React.ReactNode
   children: React.ReactNode
-  onClick?: () => void
+  type?: 'button' | 'submit' | 'reset'
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ variant = "filled", iconPosition, icon, children, onClick }) => {
-  const baseClasses = 'px-4 py-2 h-10 whitespace-nowrap rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
-  const variantClasses = variant === 'filled'
-    ? 'bg-primary text-white hover:bg-primary'
-    : 'border border-primary text-primary hover:bg-primary-50'
+const Button: React.FC<ButtonProps> = ({
+  variant = 'filled',
+  iconPosition,
+  icon,
+  children,
+  onClick,
+  type,
+}) => {
+  const baseClasses =
+    'px-4 py-2 h-10 whitespace-nowrap rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
+  const variantClasses =
+    variant === 'filled'
+      ? 'bg-primary text-white hover:bg-primary'
+      : 'border border-primary text-primary hover:bg-primary-50'
 
   return (
     <button
+      type={type}
       className={`${baseClasses} ${variantClasses} flex items-center justify-center`}
       onClick={onClick}
     >
-      {iconPosition === 'left' && icon && <span className="mr-2">{icon}</span>}
+      {iconPosition === 'left' && icon && <span className='mr-2'>{icon}</span>}
       {children}
-      {iconPosition === 'right' && icon && <span className="ml-2">{icon}</span>}
+      {iconPosition === 'right' && icon && <span className='ml-2'>{icon}</span>}
     </button>
   )
 }
