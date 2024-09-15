8718ccd2bee478e01bf90ca92b62630893dc5a34
Kennedy Martins <kennedysmartins@hotmail.com>
2024-09-14 21:18:05 -0300

    feat: :sparkles: simple initial home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,23 @@
 import Breadcrumbs from "@/components/Breadcrumbs";
 import Button from "@/components/Button";
-import Filters from "@/components/Filters";
 import NavBar from "@/components/NavBar";
-import Separator from "@/components/Separator";
-import TicketTable from "@/components/Table";
-import { BsPlus } from "react-icons/bs";
+import Link from "next/link";
+import { MdDashboard } from "react-icons/md";
 
 export default function Home() {
   return (
     <div>
-      <NavBar pageTitle="Atendimento ao cliente" />
-      <Breadcrumbs pageTitle="Atendimento ao cliente" />
-      <Separator />
-      <div className="flex gap-2 justify-start items-center px-8">
-        <Button variant="filled" iconPosition="right" icon={<BsPlus size={20} />} >Abrir ticket</Button> 
-        <Filters />
-      </div>
-        <TicketTable />
+      <NavBar pageTitle="Home" />
+      <Breadcrumbs pageTitle="Home" />
+      
+      <main className="container mx-8 mt-8">
+        <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+        <Link href="/atendimento-ao-cliente">
+          <Button variant="filled" iconPosition="left" icon={<MdDashboard size={20} />}>
+            Atendimento ao Cliente
+          </Button>
+        </Link>
+      </main>
     </div>
   );
 }
